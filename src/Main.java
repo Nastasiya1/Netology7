f244df80beb6194fac618b9dfae3eef509b224e4
Anastasiya Pomogaeva <pomogaeva.anasyasiya@gmail.com>
2023-02-07 21:05:43 +0300

    Trimmed mean method added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {1500, 3000, 6800};
+        int[] sales = {1500, 3000, 6800, 2000, 4500};
         SalesManager salesManager = new SalesManager(sales);
-        System.out.println(salesManager.max());
+        System.out.println("Максимальеая продажа - " + salesManager.max());
+        System.out.println("Обрезанное среднее значение по продажам - " + salesManager.trimmedMean());
     }
 }
