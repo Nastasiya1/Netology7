03351befba32047e911ce3e503eee64c6c0c0709
Anastasiya Pomogaeva <pomogaeva.anasyasiya@gmail.com>
2023-02-07 20:21:02 +0300

    int changed to long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {1500, 3000, 6800};
+        long[] sales = {1500, 3000, 6800, 2000, 4500};
         SalesManager salesManager = new SalesManager(sales);
         System.out.println(salesManager.max());
     }
